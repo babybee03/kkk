948826cd555afe04fb372faa3c645f120dd66e86
babybee03 <flowerysora@hanmail.net>
2024-02-23 11:59:35 +0900

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './component/Header.css';
-import Header_content from './component/Header';
-import Index_content from "./component/Index_content";
-import styles from './App.module.css';
+import Box from './component/description';
+
 
 
 function App() {
   return (
     <div>
-      <h1 className="h1_style">css 직접 import 하기</h1>
-      <p>HTML apply</p>
-      <Header_content/>
-      <h3 className={styles.module_css_01}>module css 내용입니다</h3>
-      <h4 className=".module_css_02">module 내용 2 입니다</h4>
-      <Index_content/>
-      <Index_content_02/>
+      <Box/>
     </div>
   );
 }
---
+++ src/component/description.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components'
+
+
+const Box = styled.div`
+    color: red;
+`
+
+export default () => (
+    <div>
+        <Box>
+            한국
+        </Box>
+    </div>
+
+)
